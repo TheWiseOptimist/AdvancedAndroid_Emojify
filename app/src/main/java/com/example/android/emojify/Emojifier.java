ffc03c5edf0f3c3b610705616150f8602722f752
clyde <clyder@thewiseoptimist.net>
2018-09-20 18:30:16 -0700

    update gradle dependencies

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -70,9 +70,12 @@ class Emojifier {
                 // Get the appropriate emoji for each face
                 whichEmoji(face);
 
-                // TODO (4): Create a variable called emojiBitmap to hold the appropriate Emoji bitmap and remove the call to whichEmoji()
-                // TODO (5): Create a switch statement on the result of the whichEmoji() call, and assign the proper emoji bitmap to the variable you created
-                // TODO (8): Call addBitmapToFace(), passing in the resultBitmap, the emojiBitmap and the Face  object, and assigning the result to resultBitmap
+                // TODO (4): Create a variable called emojiBitmap to hold the appropriate
+                // Emoji bitmap and remove the call to whichEmoji()
+                // TODO (5): Create a switch statement on the result of the whichEmoji() call,
+                // and assign the proper emoji bitmap to the variable you created
+                // TODO (8): Call addBitmapToFace(), passing in the resultBitmap,
+                // the emojiBitmap and the Face  object, and assigning the result to resultBitmap
 
             }
         }
